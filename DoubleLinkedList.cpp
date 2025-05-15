2d6c40ea3c7b86428988e7352ca149d3515d4956
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-15 18:38:23 +0700

    membuat class doublelinked list beserta hak akses dan membernya

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -10,6 +10,25 @@ public:
     Node *prev;
 };
 
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
+}
+
 int main()
 {
 
