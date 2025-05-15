6e565810cfad12e5efd92718e79a582222c82ec1
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-15 20:25:00 +0700

    membuat void revtraverse

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -150,6 +150,34 @@ public:
             i++;
         }
     }
+    void revtraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+        
+        // Step 1: Move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        // step 2: Traverse backward
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // Step  3: Move to previous node
+            currentNode = currentNode->prev;
+            i--;
+        }
+    }
 };
 
 int main()
